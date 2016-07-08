2da934fe40261d961646e563fab00b97be44e58f
parkj7701 <parkj7701@naver.com>
2016-07-08 16:42:47 +0900

    modified2

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello git");
 	        System.out.println("what is git");          
+		System.out.println("2002,BitKeeper");
 
 }	
 }
