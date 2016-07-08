deebb179ef0c4c76ff80156f8f3de26fee4d2563
parkj7701 <parkj7701@naver.com>
2016-07-08 16:40:50 +0900

    modified

--- Hello.java
+++ Hello.java
@@ -3,7 +3,6 @@ public class Hello{
 		System.out.println("Hello git");
 	        System.out.println("what is git");          
 		System.out.println("That`s DVCS");
-		System.out.println("2002,BitKeeper");
 
 }	
 }
